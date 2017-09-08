18bde05d297187d97acb53b225f89d2f1262b1bf
Alex Pravdin <p0stal.tomsk@gmail.com>
2017-09-08 14:24:02 +0700

    dev-999 work

--- helloworld.php
+++ helloworld.php
@@ -11,3 +11,15 @@ function getLog() {
 function serverAnswer () {
     return "\n".'And see u soon!';
 }
+
+/**
+ * Class Feature
+ */
+class Feature {
+    private $password = 'YnNhm';
+
+    public function __construct()
+    {
+        $this->password = '123';
+    }
+}
