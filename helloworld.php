d5e6b54dfef4b96b41eb5945ed4d31245d7a3ebc
Alex Pravdin <p0stal.tomsk@gmail.com>
2017-09-08 14:10:50 +0700

    dev-999 working

--- helloworld.php
+++ helloworld.php
@@ -1,8 +1,13 @@
 <?php
 
 echo 'hello world!';
+echo serverAnswer();
 
 function getLog() {
 
     var_dump('<pre>', $_SERVER);
+}
+
+function serverAnswer () {
+    return "\n".'And see u soon!';
 }
